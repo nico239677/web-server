0d6d4b35bc1e65f315cffafaac4b523519fdef54
Nicolas Mai <nicolas.mail@hec.edu>
2016-01-11 17:56:57 -0800

    Update port for heroku

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@
         next();
     },
     logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + + req.method + ' ' + req.originalUrl);
+        console.log('Request: ' + Date().toString() + + req.method + ' ' + req.originalUrl);
         next();
     }
 };
